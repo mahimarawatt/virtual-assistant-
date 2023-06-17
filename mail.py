b9757a25efc4baba53f2c8c4ee878edbb0bb8b48
Mahima Rawat <42876534+mahimarawatt@users.noreply.github.com>
2023-06-18 01:09:19 +0530

    Update mail.py

--- mail.py
+++ mail.py
@@ -1,6 +1,2 @@
-senderemail = 'mahimarawat223@gmail.com'
-epwd = 'jffqsqmxtsflpaiu'
-# "dad": "+917797499440"
-# "naga": "+917019460049"
-# "sachin": "+918296600633"
-# "ravi shankar sir" :"+919886429843"}S
+senderemail = ""
+password = ""
